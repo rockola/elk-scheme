6cdd576cb050c7dbe4aba243d0765f30b68b1e46
Ola Rinta-Koski <github@rinta-koski.net>
2022-01-15 16:48:16 +0200

    C files in xwidgets/ deleted -- generated from .d files

--- lib/xwidgets/motif/bulletin-brd.c
+++
@@ -1,9 +0,0 @@
-#include "../xt.h"
-#include <Xm/Xm.h>
-#include <Xm/BulletinB.h>
-
-void elk_init_motif_bulletinboard () {
-    XtResourceList r = 0;
-    Define_Class ("bulletin-board", xmBulletinBoardWidgetClass, r, 0);
-    P_Provide(Intern("motif:bulletin-brd"));
-}
--- lib/xwidgets/xaw/smebsb.c
+++
@@ -1,9 +0,0 @@
-#include "../xt.h"
-#include <X11/Xaw/SmeBSB.h>
-
-void elk_init_xaw_smebsb () {
-    XtResourceList r = 0;
-    Define_Class ("smebsb", smeBSBObjectClass, r, 0);
-    Define_Callback ("smebsb", "callback", 0);
-    P_Provide(Intern("xaw:smebsb"));
-}
